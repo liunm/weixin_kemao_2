e27d97b53a0bbab5c0393a6597b363738371b353
ASUS <ASUS@DESKTOP-K3D47FF>
2019-07-05 16:14:27 +0800

    接收微信公众号平台发送给我们的消息

--- src/main/java/org/fkjava/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/fkjava/weixin/controller/MessageReceiverController.java
@@ -1,14 +1,19 @@
 package org.fkjava.weixin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/kemao_2/message/receiver")
- 
 public class MessageReceiverController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
 	@GetMapping
 	public String echo(//
 			@RequestParam("signature") String signature,//
@@ -16,10 +21,23 @@ public class MessageReceiverController {
 			@RequestParam("nonce") String nonce,//
 			@RequestParam("echostr") String echostr//
 			
-			
 	//
      )  {
 	return echostr;
 	}
+	
+	
+	@PostMapping
+	public String onMessage(//
+			@RequestParam("signature") String signature,//
+			@RequestParam("timestamp") String timestamp,//
+			@RequestParam("nonce") String nonce,//
+			@RequestBody String xml ) {
+		LOG.trace("收到的消息原文:\n{}\n----------------", xml);
+		return"";
+		
+	}
+	
+	
 }
 
